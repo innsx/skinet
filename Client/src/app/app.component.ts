70370c697e632da5993c0a5b8ca40a5302857073
Nick Xiong <nickxiong2030innsx@gmail.com>
2023-11-22 18:59:47 -0500

    End of Section/module 8 Adding Angular Components and Module commits

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './shared/models/product';
+import { IPagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Welcome to SkiNet App!';
+
+  products: IProduct[] = [];
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.http.get<IPagination>('https://localhost:5001/api/products').subscribe((response: IPagination) => {
+      console.log('Products: ', response);
+      this.products = response.data;
+    }, error => {
+      console.error('Error: ', error);
+    });
+  }
+
 }
---
+++ Client/src/app/shop/shop.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { IPagination } from '../shared/models/pagination';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShopService {
+  private apiUrl: 'https://localhost:5001/api/';
+
+  constructor(private http: HttpClient) {
+  }
+
+  getProducts() {
+    return this.http.get<IPagination>(this.apiUrl + 'products');
+  }
+}
